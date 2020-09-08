ffef0ce4d14ec34042db94d51889157589c6af85
Pratik-Gosatwar <70031342+Pratik-Gosatwar@users.noreply.github.com>
2020-09-08 14:44:13 +0530

    Project has been added

---
+++ WebApplication1/Final.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Final : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                lblmsg.Text = "Yor are on Final page";
+            }
+        }
+    }
+}
---
+++ WebApplication1/hOME.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class hOME : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                ViewState["Counter"] = 0;
+            }
+        }
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int i = (int)ViewState["Counter"];
+            i += 1;
+
+            ViewState["Counter"] = i;
+
+            TextBox1.Text = i.ToString();
+        }
+    }
+}
